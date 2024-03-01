e17277192f64876913fe3cbed8c4bc734be581a2
neutrinobolt <bescsn@gmail.com>
2024-03-01 15:27:24 -0800

    test add function

--- Assignments/Assignment6/main.cpp
+++ Assignments/Assignment6/main.cpp
@@ -9,6 +9,19 @@ using std::endl;
 int main() {
     BinSTree *testTree = new BinSTree;
 
+    ////////////////////////////////////////////////////////////////
+    // Test add function
+    // add root node, check it's there
+    testTree->add(20);
+    cout << "testTree->root->value: " << testTree->root->value << endl;
+    // add node left of root, check it's there
+    testTree->add(10);
+    cout << "testTree->root->left->value: " << testTree->root->left->value << endl;
+    // add node right of root, check it's there
+    testTree->add(30);
+    cout << "testTree->root->right->value: " << testTree->root->right->value << endl;
+
+
     // Garbage Collection
     testTree->~BinSTree();
 
